6d812ae90c1a69972c2814b06a81b0f060a5001b
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-03-26 21:22:56 -0400

    added/modified bookjs,userjs,serverjs, testing on robo3t and postman, server

--- server/server.js
+++ server/server.js
@@ -8,13 +8,78 @@ const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE)
 
+const { User } = require('./models/user');
+const { Book } = require('./models/book');
+
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 //creating routes
 
+//==============GET=======================//
+app.get('/api/getBook',(req,res)=> {
+  let id = req.query.id;
+
+  Book.findById(id,(err,doc)=>{
+    if(err) return res.status(400).send(err);
+    res.send(doc);
+  })
+})
+
+app.get('/api/books', (req,res) => {
+  // localhost:3001/api/books?skip=3&limit=2&order=asc
+  let skip = parseInt(req.query.skip)
+  let limit = parseInt(req.query.limit);
+  let order = req.query.order;
+
+
+  // Order = asc || desc
+  Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err,doc)=>{
+    if(err) return res.status(400).send(err);
+    res.send(doc);
+  })
+})
+
+
+
+//==============POST=======================//
+app.post('/api/book', (req,res) => {
+  const book = new Book(req.body)
+
+  book.save((err,doc) => {
+    if(err) return res.status(404).send(err);
+    res.status(200).json({
+      post:true,
+      bookId: doc._id
+    })
+  })
+
+})
+
+//==============UPDATE=======================//
+app.post('/api/book_update',(req,res)=>{
+  Book.findByIdAndUpdate(req.body._id,req.body,{new:true},(err,doc)=>{
+    if(err) return res.status(400).send(err);
+    res.json({
+      success:true,
+      doc
+    })
+  })
+})
+
+//==============DELETE=======================//
+app.delete('/api/delete_book', (req,res)=>{
+  let id = req.query.id;
+  Book.findByIdAndRemove(id,(err,doc)=>{
+    if(err) return res.status(400).send(err);
+    res.json(true)
+  })
+})
+
+
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=> {
-  console.log('SERVER IS RUNNING')
+  console.log('SERVER IS RUNNING, RELAX AND TAKE IT EASY')
 })
