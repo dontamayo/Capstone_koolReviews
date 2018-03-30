f78cb0143f11d0bca806045109002c06a9e00de8
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-03-29 20:44:18 -0400

    more work, its not functioning yet && git push

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -3,43 +3,42 @@ import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router-dom';
 import Nav from './Sidenav/sidenav';
 
-
 class Header extends Component {
 
-  state = {
-    showNav:false
-  }
+    state = {
+        showNav:false
+    }
 
-  onHideNav = () => {
-    this.setState({showNav:false})
-  }
-  render(){
-    return(
-        <header>
-          <div className="open_nav">
-            <FontAwesome name="bars"
-              onClick={()=>this.setState({showNav:true})}
-              style={{
-                color:'#ffffff',
-                padding:'10px',
-                cursor:'pointer'
-              }}
-              />
-          </div>
+    onHideNav = () => {
+        this.setState({showNav:false})
+    }
 
-            <Nav
-              showNav={this.state.showNav}
-              onHideNav={()=>this.onHideNav()}
-              />
 
-            <Link to="/" className="logo">
-              Kool Reviews
-            </Link>
+    render() {
+        return (
+            <header>
+                <div className="open_nav">
+                    <FontAwesome name="bars"
+                        onClick={()=> this.setState({showNav:true})}
+                        style={{
+                            color:'#ffffff',
+                            padding:'10px',
+                            cursor:'pointer'
+                        }}
+                    />
+                </div>
+                <Nav
+                    showNav={this.state.showNav}
+                    onHideNav={()=>this.onHideNav()}
+                />
 
-        </header>
+                <Link to="/" className="logo">
+                        Kool Reviews 2
+                </Link>
 
-    );
-  }
+            </header>
+        );
+    }
 }
 
 export default Header;
--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HomeContainer from '../../containers/home_container'
 
-const Home = (props) => {
+const Home = () => {
   return (
     <div>
       <HomeContainer/>
